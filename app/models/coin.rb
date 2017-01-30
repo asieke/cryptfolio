8e1e4479b892960bec2f76e5bf7196e4fbca1ea2
asieke <asieke@gmail.com>
2017-01-29 16:40:25 -0800

    updated db:seeds

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -5,4 +5,13 @@ class Coin < ApplicationRecord
 	def coinmarketcaplink
 		return "https://coinmarketcap.com/currencies/" + currency_id
 	end
+
+	def current_price
+		puts "=======\n"
+		puts id
+		puts "========\n"
+
+		p1 = Price.where(["coin_id = ?", self.id]).order("query_time DESC").first
+		return p1.price_usd
+	end
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,5 @@
 class Price < ApplicationRecord
   belongs_to :coin
+  validates_uniqueness_of :query_time
+
 end
